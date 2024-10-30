cce030c60ad71ffcc5f76b663431e7d9f01b7155
pramodchauhanting <pramod.chouhan@ting.in>
2024-10-30 18:09:47 +0530

    changes done

--- pages/index.js
+++ pages/index.js
@@ -4,38 +4,41 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import MetaTags from "@/components/MetaTags";
+import BannerScrollTxt from "../src/components/BannerScrollTxt";
 
 // SECTIONS //
 
 // PLUGINS //
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 // UTILS //
 
 // STYLES //
 import styles from "@/styles/pages/Home.module.scss";
-
 // IMAGES //
 
 // DATA //
 
 /** Home Page */
 export default function HomePage() {
+	gsap.registerPlugin(ScrollTrigger);
 	return (
 		<div>
 			{/* Metatags */}
 			<MetaTags Title={"Home"} Desc={"Home Desc"} OgImg={""} Url={"/"} />
 
 			{/* Header */}
-			<Header />
+			{/* <Header /> */}
 
 			{/* Page Content starts here */}
-			<main className={`${styles.HomePage} bg_primary`}>
-				<h2>This is ting base next.js template</h2>
+			<main className={`${styles.HomePage}`}>
+				<BannerScrollTxt gsap={gsap} ScrollTrigger={ScrollTrigger} />
 			</main>
 			{/* Page Content ends here */}
 
 			{/* Footer */}
-			<Footer />
+			{/* <Footer /> */}
 		</div>
 	);
 }
---
+++ src/components/BannerScrollTxt.js
@@ -0,0 +1,71 @@
+// MODULES //
+import React, { useState, useEffect, useRef } from "react";
+
+// COMPONENTS //
+import Image from "next/image";
+import Link from "next/link";
+
+// SECTIONS //
+
+// PLUGINS //
+
+// UTILS //
+
+// STYLES //
+import styles from "@/styles/components/BannerScrollTxt.module.scss";
+
+// IMAGES //
+import Arrow from "@/../public/img/icons/arrow.svg";
+
+// DATA //
+
+/** BannerScrollTxt Component */
+export default function BannerScrollTxt({ gsap, ScrollTrigger }) {
+	useEffect(() => {
+		const toastAnimTimeline = gsap.timeline({});
+		const winW = window.innerWidth;
+		const winH = window.innerHeight;
+
+		/** animation for desktop */
+		function toastAnimationDesktop() {
+			toastAnimTimeline
+				.fromTo(".aniTxt", { y: "100vh", color:"black", opacity: 0 }, { y: "0vh", opacity: 1 }, 0);
+			ScrollTrigger.create({
+				trigger: `.${styles.BannerScrollTxt}`,
+				animation: toastAnimTimeline,
+				start: "top top",
+				end: "bottom center",
+				scrub: true,
+				pin: true,
+				markers: true,
+				pinSpacing: true,
+			});
+		}
+
+		/** Check if the window width is greater than a specific breakpoint (e.g., 768px) */
+		// if (winW > 1199) {
+		// 	/** Desktop view */
+			
+		// }
+		toastAnimationDesktop();
+	}, []);
+	return (
+		<div className={`${styles.abc}`}>
+			<section className={`${styles.BannerScrollTxtTwo}`}>
+				
+			</section>
+			<section className={`${styles.BannerScrollTxt}`}>
+				<div className={`${styles.aniTxt} aniTxt`}>
+					<h2>
+						Demo Text
+					</h2>
+				</div>
+				<div className={`${styles.bubbles} bubbles`}></div>
+			</section>
+			<section className={`${styles.BannerScrollTxtTwo}`}>
+				
+			</section>
+		</div>
+		
+	);
+}
